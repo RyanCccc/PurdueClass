f261b5030de458bc3ec552d3d76eb08fb1c29e3b
RyanCccc <chenrd769@gmail.com>
2014-03-08 02:52:29 -0500

    Added more url

--- course/urls.py
+++ course/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('course.views',
-    url(r'subject/(?P<subject>[a-z]+)/$', 'subject'),
-    url(r'subject/(?P<subject>[a-z]+)/(?P<CNBR>[0-9a-z]+)/$', 'courses'),
+    url(r'subject/(?P<subject>[a-zA-Z]+)/$', 'subject'),
+    url(r'subject/(?P<subject>[a-zA-Z]+)/(?P<CNBR>[0-9a-zA-Z]+)/$', 'courses'),
 )
