78ec1d0f816eae967e851fac13ed5c36f3d492d8
RyanCccc <chenrd769@gmail.com>
2014-03-28 20:20:44 -0400

    Fixed path

--- pu_class/views.py
+++ pu_class/views.py
@@ -2,11 +2,12 @@
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
 from util import convert_code_to_term, convert_term_to_code
+from PurdueClass.settings import PROJECT_DIR
 import json
 
 @csrf_exempt
 def get_all_by_term(request, term):
     term_code = convert_term_to_code(term)
-    f = open('pu_class/%s.json'%term_code, 'r')
+    f = open('%s/pu_class/%s.json'%(PROJECT_DIR,term_code), 'r')
     courses_j = f.read()
     return HttpResponse(courses_j, content_type="application/json")
