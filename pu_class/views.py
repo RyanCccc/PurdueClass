8da7da3907d1a3a5daf5b4c27591a70339893fcd
RyanCccc <chenrd769@gmail.com>
2014-03-28 20:16:16 -0400

    Fixed some bugs

--- pu_class/views.py
+++ pu_class/views.py
@@ -5,8 +5,8 @@ from util import convert_code_to_term, convert_term_to_code
 import json
 
 @csrf_exempt
-def term(request, term):
+def get_all_by_term(request, term):
     term_code = convert_term_to_code(term)
-    f = open('%s.json'%term_code, 'r')
+    f = open('pu_class/%s.json'%term_code, 'r')
     courses_j = f.read()
     return HttpResponse(courses_j, content_type="application/json")
