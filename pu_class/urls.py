f5e2a2ae836a51a2b8e18b5e135a4156a882ba3f
RyanCccc <chenrd769@gmail.com>
2014-04-09 23:33:57 -0400

    Added seats check

--- pu_class/urls.py
+++ pu_class/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('pu_class.views',
     url(r'all/(?P<term>[a-zA-Z0-9]+)/$', 'get_all_by_term'),
     url(r'catalogs/(?P<term>[a-zA-Z0-9]+)/$', 'get_all_cat_by_term'),
+    url(r'seats/$', 'get_seats_by_term'),
 )
--- pu_class/views.py
+++ pu_class/views.py
@@ -1,11 +1,11 @@
 # Create your views here.
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
-from util import convert_code_to_term, convert_term_to_code
+from util import convert_code_to_term, convert_term_to_code, get_seats
 from PurdueClass.settings import PROJECT_DIR
 import json
 
-@csrf_exempt
+
 def get_all_by_term(request, term):
     term_code = convert_term_to_code(term)
     f = open('%s/pu_class/%s.json'%(PROJECT_DIR,term_code), 'r')
@@ -17,3 +17,21 @@ def get_all_cat_by_term(request, term):
     f = open('%s/pu_class/%s_catalogs.json'%(PROJECT_DIR,term_code), 'r')
     courses_j = f.read()
     return HttpResponse(courses_j, content_type="application/json")
+
+@csrf_exempt
+def get_seats_by_term(request):
+    term = request.GET.get('term')
+    crn = request.GET.get('crn')
+    if not term or not crn:
+        return HttpResponse('')
+    term_code = convert_term_to_code(term)
+    max_num, taken_num = get_seats(crn, term_code)
+    remain_num = max_num - taken_num
+    ##seats = {
+    ##    'max':max_num,
+    ##    'remain':remain_num,
+    ##    'taken':taken_num,
+    ##}
+    ##seats_j = json.dumps(seats)
+    _str = 'Capacity: %d\nActual: %d\nRemaining: %d' % (max_num, taken_num, remain_num);
+    return HttpResponse(_str)
